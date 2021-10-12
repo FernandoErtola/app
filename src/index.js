662f63b6514b9eeed099f129933d3eb9b43cd55d
Fernando Ertola <fernandoertola@gmail.com>
2021-10-12 20:34:33 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react"
 
 import ReactDOM from "react-dom"
 
-const Elemento = () => <p>Hola Mundo</p>
+const Elemento = () => <p>Hola Mundo del React</p>
 
 ReactDOM.render(<Elemento/> , document.getElementById("root") )
