6d61d3068cd6e2502659e5d641019ce05315130f
Fernando Ertola <fernandoertola@gmail.com>
2021-10-22 10:07:35 -0300

    ilc

--- src/app.js
+++ src/app.js
@@ -1,21 +1,27 @@
+
 import Header from "./header"
 import Footer from "./footer"
 import ItemListContainer from "./itemListContainer"
 
 
-const General = () => {
+const App = () => {
+
     return(
         <>
+                       
             <Header 
                 nombre="Fernando" 
                 apellido="Ertola" 
                 edad={35} 
             />
-            <ItemListContainer/>
+            <ItemListContainer
+                nombre="Fernando" 
+                apellido="Ertola" 
+                edad={35} 
+            />
             <Footer/>
         </>
     )
 }
 
-
-export default General
+export default App
--- src/itemListContainer.js
+++ src/itemListContainer.js
@@ -1,8 +1,30 @@
-const ItemListContainer = () => {
+import {useState} from "react"
+
+const ItemListContainer = ({nombre, apellido, edad}) => {
+    
+/*      let contador = 0
+ */ 
+/*     const resultado = useState("Hola")
+    const estado = resultado[0]
+    const setEstado = resultado [1]  */
+    const [saludo,setSaludo] = useState("Bienvenido")
+    const [contador, setContador] = useState(0)
+
+    const Acumula = () => {
+        
+        setContador(contador + 1)
+        
+   }
+   console.log(contador)
     return(
+        <>
+        <button onClick={Acumula}>click</button>
+
         <main className="ilc">
-            <p>Hola Mundo</p>
+            <p>{saludo} <a className="user">{nombre} ({edad})</a></p>
+            <p>{contador}</p>
         </main>
+        </>
     )
 }
 
